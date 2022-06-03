18a35571d8e595626da395f2da21a7d00fc166d3
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-06-03 23:51:32 +0100

    Removed Public files

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublicComponent } from './public.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-
 
 @NgModule({
   declarations: [
-    PublicComponent,
-    NavbarComponent,
-    ContentComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule

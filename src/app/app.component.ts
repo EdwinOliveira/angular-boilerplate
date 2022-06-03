73beadd557de079f712240ac39eaabe25e18caf2
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-06-03 23:42:19 +0100

    Removed initialized @App files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: "",
 })
-export class AppComponent {
-  title = 'angular-boilerplate';
-}
+export class AppComponent { }

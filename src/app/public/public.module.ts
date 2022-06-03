c82d9553dc458052c818650344df369e34478da6
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-06-03 23:49:44 +0100

    Added Layout Components

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PublicComponent } from './public.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublicComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]

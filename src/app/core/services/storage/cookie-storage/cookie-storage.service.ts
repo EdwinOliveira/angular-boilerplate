9707a91a97dd3a8422b0f13c29c6d14ef418eded
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-06-04 00:49:45 +0100

    Refixed services

--- src/app/core/services/storage/cookie-storage/cookie-storage.service.ts
+++ src/app/core/services/storage/cookie-storage/cookie-storage.service.ts
@@ -1,31 +1,8 @@
 import { Injectable } from '@angular/core';
-import { StorageService } from 'src/app/shared/abstracts/storage.service.abstract';
-import { TokenCollection } from 'src/app/shared/constants/collections/token.collection';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CookieStorageService extends StorageService {
-
-  constructor() {
-    super();
-  }
-
-  public override fetchToken<T>(name: TokenCollection): T | null {
-    const token = localStorage.getItem(String(name));
-
-    return token === null ? null : JSON.parse(token);
-  };
-
-  public override createToken<T>(name: TokenCollection, data: T): void {
-    return localStorage.setItem(String(name), JSON.stringify(data));
-  };
-
-  public override removeToken(name: TokenCollection): void {
-    return localStorage.removeItem(String(name));
-  };
-
-  public override clear(): void {
-    return localStorage.clear();
-  };
+export class CookieStorageService {
+  public constructor() { }
 }
--- src/app/core/services/storage/local-storage/local-storage.service.ts
+++ src/app/core/services/storage/local-storage/local-storage.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from 'src/app/shared/abstracts/storage.service.abstract';
+import { TokenCollection } from 'src/app/shared/constants/collections/token.collection';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LocalStorageService {
+export class LocalStorageService extends StorageService {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
+
+  public override fetchToken<T>(name: TokenCollection): T | null {
+    const token = localStorage.getItem(String(name));
+
+    return token === null ? null : JSON.parse(token);
+  };
+
+  public override createToken<T>(name: TokenCollection, data: T): void {
+    return localStorage.setItem(String(name), JSON.stringify(data));
+  };
+
+  public override removeToken(name: TokenCollection): void {
+    return localStorage.removeItem(String(name));
+  };
+
+  public override clear(): void {
+    return localStorage.clear();
+  };
 }
